7d199b4f75fb50b3679820d4bfbef1d9620838d1
minjung <minjungx2@gmail.com>
2024-09-19 20:29:56 +0900

    Props 기초

--- 01_basic/src/App.jsx
+++ 01_basic/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImage from './assets/react-core-concepts.png'
+import componentsImg from './assets/components.png'
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -19,12 +20,32 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}/>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI buildong block."
+              image={componentsImg}
+            />
+          </ul>
+        </section>
+        
       </main>
     </div>
   );
