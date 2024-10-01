5dfb3d901020685a72f413b101924c7b2ccf7d84
minjung <minjungx2@gmail.com>
2024-10-01 16:30:00 +0900

    State

--- 01_basic/src/App.jsx
+++ 01_basic/src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import {CORE_CONCEPTS} from './data.js'
 import Header from './components/Header/Header.jsx'
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
+  let [selectedTopic, setSelectedTopic] = useState('components')
+
   function handleSelect(selectedButton) {
-    console.log(selectedButton)
+    setSelectedTopic(selectedButton)
   }
 
   return (
@@ -33,6 +37,15 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
